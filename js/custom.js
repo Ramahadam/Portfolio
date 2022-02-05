3158819d3fce0592610bf921ab6d6922e6312ed9
Ramahadam <ramy.adam33@gmail.com>
2022-02-05 15:24:00 +0400

    Adding owl carsousel

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,26 @@ let btn = document.querySelector(".nav-toggler");
 btn.addEventListener('click',function(){
     navLists.classList.toggle("active");
 });
+
+
+//Owl carousel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoPlaySpeed: 5000,
+    autoPlayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
